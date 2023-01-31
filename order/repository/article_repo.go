14f9aede0d80dbd18324060cbf2f6223829072fe
fadelmp <fadelmpuluhulawa@gmail.com>
2023-01-31 17:57:33 +0700

    Change Service to Services on Preload Article

--- order/repository/article_repo.go
+++ order/repository/article_repo.go
@@ -24,7 +24,7 @@ func (a *ArticleRepository) GetAll() []entity.Article {
 	var articles []entity.Article
 
 	a.DB.Model(&entity.Article{}).Order("id asc").
-		Preload("Order").Preload("Status").Preload("Service").
+		Preload("Order").Preload("Status").Preload("Services").
 		Find(&articles)
 
 	return articles
@@ -35,7 +35,7 @@ func (a *ArticleRepository) GetByID(id uint) entity.Article {
 	var article entity.Article
 
 	a.DB.Where("id=?", id).Order("id asc").
-		Preload("Order").Preload("Status").Preload("Service").
+		Preload("Order").Preload("Status").Preload("Services").
 		Find(&article)
 
 	return article
