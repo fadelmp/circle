bddd62e7109e69db1b2137eeb07d92d33fc1a52c
fadelmp <fadelmpuluhulawa@gmail.com>
2023-02-01 11:16:44 +0700

    Add Order Unit Routes

--- core/routes/route.go
+++ core/routes/route.go
@@ -28,6 +28,10 @@ func Init(routes *echo.Echo) *echo.Echo {
 	order_status := injection.OrderStatusInjection()
 	OrderStatusRoutes(routes, order_status)
 
+	// Order Unit Route & Injection
+	order_units := injection.OrderUnitInjection()
+	OrderUnitRoutes(routes, order_units)
+
 	// Ordeer Article & Injection
 	order_article := injection.OrderArticleInjection()
 	OrderArticleRoutes(routes, order_article)
