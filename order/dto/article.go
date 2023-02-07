eb2d221bd3460d5277f0d6354855c59188bc697b
fadelmp <fadelmpuluhulawa@gmail.com>
2023-02-07 22:09:38 +0700

    Change Service json key

--- order/dto/article.go
+++ order/dto/article.go
@@ -2,16 +2,16 @@ package dto
 
 // Database Design
 type Article struct {
-	ID         uint   `json:"id"`
-	OrderID    uint   `json:"order_id"`
-	Name       string `json:"name"`
-	Quantity   int    `json:"quantity"`
-	UnitID     uint   `json:"unit_id"`
-	UnitName   string `json:"unit_name"`
-	Amount     int    `json:"amount"`
-	Note       string `json:"note"`
-	Path_Image string `json:"path_image"`
-	StatusID   uint   `json:"status_id"`
-	StatusName string `json:"status_name"`
-	Services   []Service
+	ID         uint      `json:"id"`
+	OrderID    uint      `json:"order_id"`
+	Name       string    `json:"name"`
+	Quantity   int       `json:"quantity"`
+	UnitID     uint      `json:"unit_id"`
+	UnitName   string    `json:"unit_name"`
+	Amount     int       `json:"amount"`
+	Note       string    `json:"note"`
+	Path_Image string    `json:"path_image"`
+	StatusID   uint      `json:"status_id"`
+	StatusName string    `json:"status_name"`
+	Services   []Service `json:"services"`
 }
